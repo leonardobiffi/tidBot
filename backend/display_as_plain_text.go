868e65cf45a9288da45a6d07c5a9fbfdba86dbdb
Leonardo Biffi <leonardo.biffi@bancointer.com.br>
2022-03-11 12:04:09 -0300

    format body msg

--- backend/display_as_plain_text.go
+++ backend/display_as_plain_text.go
@@ -7,24 +7,26 @@ import (
 
 func FormatAsPlainText(info WeatherInfo) (text string) {
 	var body []string
-	body = append(body, fmt.Sprintf("📍 Location: %s\n", info.location()))
-	if len(info.Kind) > 0 {
-		body = append(body, fmt.Sprintf("Weather description: %s\n", info.Kind[0].Description))
-	}
+	body = append(body, "🌦️ Weather Now\n\n")
+
+	body = append(body, fmt.Sprintf("🌍 Location: %s\n", info.location()))
+	// if len(info.Kind) > 0 {
+	// 	body = append(body, fmt.Sprintf("Weather description: %s\n", info.Kind[0].Description))
+	// }
 	body = append(body, fmt.Sprintf("🌡️ Temperature: %.2f °C\n", info.Main.Temperature))
-	body = append(body, fmt.Sprintf("Temperature feels like: %.2f °C\n", info.Main.TemperatureFeelsLike))
-	body = append(body, fmt.Sprintf("Temperature min: %.2f °C\n", info.Main.TemperatureMin))
-	body = append(body, fmt.Sprintf("Temperature max: %.2f °C\n", info.Main.TemperatureMax))
-	body = append(body, fmt.Sprintf("Wind direction: %d°\n", info.Wind.Direction))
+	body = append(body, fmt.Sprintf("🌡️ Temperature feels like: %.2f °C\n", info.Main.TemperatureFeelsLike))
+	body = append(body, fmt.Sprintf("⬇️ Temperature min: %.2f °C\n", info.Main.TemperatureMin))
+	body = append(body, fmt.Sprintf("⬆️ Temperature max: %.2f °C\n", info.Main.TemperatureMax))
+	// body = append(body, fmt.Sprintf("Wind direction: %d°\n", info.Wind.Direction))
 	body = append(body, fmt.Sprintf("🌬️ Wind speed: %.2f km/h\n", convertToKmPerH(info.Wind.Speed)))
-	body = append(body, fmt.Sprintf(
-		"Wind gust speed: %.2f km/h\n",
-		convertToKmPerH(info.Wind.GustSpeed),
-	))
-	body = append(body, fmt.Sprintf("Visibility: %d m\n", info.Visibility))
+	// body = append(body, fmt.Sprintf(
+	// 	"Wind gust speed: %.2f km/h\n",
+	// 	convertToKmPerH(info.Wind.GustSpeed),
+	// ))
+	// body = append(body, fmt.Sprintf("Visibility: %d m\n", info.Visibility))
 	body = append(body, fmt.Sprintf("🌧️ Precipitation volume: %.2f mm/h\n", info.precipitationVolume()))
-	body = append(body, fmt.Sprintf("Pressure: %d hPa\n", info.Main.Pressure))
-	body = append(body, fmt.Sprintf("Humidity: %d %%\n", info.Main.Humidity))
+	// body = append(body, fmt.Sprintf("Pressure: %d hPa\n", info.Main.Pressure))
+	body = append(body, fmt.Sprintf("💧 Humidity: %d %%\n", info.Main.Humidity))
 
 	return strings.Join(body, "")
 }
